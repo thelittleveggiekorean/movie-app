9d7fe1a621335a0312b3157574f32fc765285ca6
Chohee <chohee4421@gmail.com>
2022-05-31 19:35:27 +0900

    Updates

---
+++ precache-manifest.f6ba592b4c55e1fc59ff300d46a55ecb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd02fa7da1f8e09f88a3",
+    "url": "/movie-app/static/css/main.77336b16.chunk.css"
+  },
+  {
+    "revision": "fd02fa7da1f8e09f88a3",
+    "url": "/movie-app/static/js/main.fd02fa7d.chunk.js"
+  },
+  {
+    "revision": "84a68f59657befc8c008",
+    "url": "/movie-app/static/js/1.84a68f59.chunk.js"
+  },
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "7b2641b1198381bab0f631f376309357",
+    "url": "/movie-app/index.html"
+  }
+];
