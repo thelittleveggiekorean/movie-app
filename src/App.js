6e9379af3580321bf97ea768e7510bb37b1290f3
Chohee <chohee4421@gmail.com>
2022-05-31 12:47:40 +0900

    Show movie data from API

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
     const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&limit=10`
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.5&sort_by=year`
     );
     const json = await response.json();
     setMovies(json.data.movies);
@@ -15,7 +15,28 @@ function App() {
     getMovies();
   }, []);
   console.log(movies);
-  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} alt={movie.title}></img>
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((genre) => (
+                  <li key={genre}>{genre}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
