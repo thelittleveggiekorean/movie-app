7d54d4a8065459f513bc19d83f721fbf07915754
Chohee <chohee4421@gmail.com>
2022-05-31 12:35:07 +0900

    Improve fetch to async and await

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&limit=10`
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&limit=10`)
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
   console.log(movies);
   return <div>{loading ? <h1>Loading...</h1> : null}</div>;
