08ca84fd0ecb72bf879c4022d153aa6af1f46baf
Chohee <chohee4421@gmail.com>
2022-05-31 12:27:39 +0900

    Add a page loader and fetch movie data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&limit=10`)
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+  }, []);
+  console.log(movies);
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
 export default App;
